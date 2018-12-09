b6afd6099ad0117c82f832cc275d72d9ef400abb
linareyes <42820402+linareyes@users.noreply.github.com>
2018-12-09 12:32:16 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).foundation()
+
+
+
+
+$(document).mouseup(function (e) {
+  var container = $('#contact-panel');
+  if (!container.is(e.target) // if the target of the click isn't the container...
+  && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.removeClass('is-active');
+    }
+});
+
+
+
+$(document).ready(function() {
+	// Users can skip the loading process if they want.
+	$('.skip').click(function() {
+		$('.overlay, body').addClass('loaded');
+	})
+
+	// Will wait for everything on the page to load.
+	$(window).bind('load', function() {
+		$('.overlay, body').addClass('loaded');
+		setTimeout(function() {
+			$('.overlay').css({'display':'none'})
+		}, 5000)
+	});
+
+	// Will remove overlay after 1min for users cannnot load properly.
+	setTimeout(function() {
+		$('.overlay, .body').addClass('loaded');
+	}, 60000);
+})
